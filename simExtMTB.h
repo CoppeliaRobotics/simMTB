b7e96a475ac31f82f02e42c2e796c96e8461fbdf
Coppelia <mfreese@gmx.ch>
2023-04-11 16:53:14 +0200

    small refact + fvisibility=hidden

--- simExtMTB.h
+++ simExtMTB.h
@@ -1,14 +1,7 @@
 #pragma once
 
-#ifdef _WIN32
-    #define SIM_DLLEXPORT extern "C" __declspec(dllexport)
-#endif
-#if defined (__linux) || defined (__APPLE__)
-    #define SIM_DLLEXPORT extern "C"
-#endif
+#include <simLib/simExp.h>
 
-
-// The 3 required entry points of the CoppeliaSim plugin:
 SIM_DLLEXPORT unsigned char simStart(void* reservedPointer,int reservedInt);
 SIM_DLLEXPORT void simEnd();
 SIM_DLLEXPORT void* simMessage(int message,int* auxiliaryData,void* customData,int* replyData);
